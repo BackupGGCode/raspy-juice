a284945383e5c5e94d7379ca79d4984646e564c2
adnan@singnet.com.sg <adnan@singnet.com.sg@18edbb5f-9ea4-c225-5032-a7d48193d222>
2012-06-08 10:26:38 +0000

    Bootloader fails in RPi - Added AVR Studio 4 project and testing with direct ICSP flashing of test application.

---
+++ trunk/tests/rs485.c
@@ -0,0 +1,64 @@
+#include "juice.h"
+#include <avr/interrupt.h>
+
+#define MYUBRR (F_CPU/16/BAUD-1)
+
+void rs485_init(unsigned long baud)
+{
+	/* Set baud rate */
+	unsigned long ubrr = (F_CPU / 16 / baud - 1);
+	UBRR0H = (unsigned char)(ubrr>>8);
+	UBRR0L = (unsigned char)(ubrr>>0);
+	/* Enable receiver and transmitter */
+	UCSR0B = (1<<RXEN0)|(1<<TXEN0);
+	/* Set frame format: Async,8,N,1 */
+	UCSR0C = 0b00000110;
+	RS485_TXEN_OFF();
+}     
+
+void rs485_off(void)
+{
+	UCSR0B = 0;
+}
+
+char rs485_havechar(void)
+{
+	return (UCSR0A & (1<<RXC0));
+}
+
+char rs485_getc(void)
+{
+	//TX485_OFF();
+	while (!rs485_havechar())
+		;
+	return UDR0;
+}
+
+void rs485_putc(char c)
+{
+	RS485_TXEN_ON();
+	loop_until_bit_is_set(UCSR0A, UDRE0);
+
+	UCSR0B = (1<<TXEN0);
+
+	UDR0 = c;
+	UCSR0A |= (1<<TXC0);
+	loop_until_bit_is_set(UCSR0A, TXC0);
+	RS485_TXEN_OFF();
+
+	UCSR0B = (1<<RXEN0);
+
+}
+
+int rs485_putchar(char c, FILE *stream)
+{
+	if (c == '\n')
+		rs485_putc('\r');
+	rs485_putc(c);
+	return 0;
+}
+
+int rs485_getchar(FILE *stream)
+{
+	return rs485_getc();
+}
