cc341c418414d19cf83734500c1bfcf260d2f67f
adnan@singnet.com.sg <adnan@singnet.com.sg@18edbb5f-9ea4-c225-5032-a7d48193d222>
2012-06-15 08:30:51 +0000

    Modified and expanded loop-test to display RJ status, read/write RS232/485 interfaces, and set servo PWMs.

--- tests/loop-test.c
+++ tests/loop-test.c
@@ -1,18 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <linux/i2c-dev.h>
 #include <linux/fcntl.h>
+
+#define GSTAT	0x00
+#define RXA232	0x01	/* RS232 data available */
+#define RXA485	0x04	/* RS485 data available */
+#define EEBUSY	0x20	/* EEPROM write in progress */
+#define ADCBUSY	0x40	/* ADC conversion in progress */
+#define RS232D	0x10
+#define RS485D	0x20
+#define SERVO_0 0x01
+#define SERVO_1 0x02
+#define SERVO_2 0x03
+#define SERVO_3 0x04
+
+int file;
+int rj_readstat(void);
+int rj232_getc(void);
+int rj485_getc(void);
+int rj232_read(void);
+int rj485_read(void);
+int rj232_send(unsigned char *buf, int len);
+int rj485_send(unsigned char *buf, int len);
+int rj_setservo(int chan, int usec);
+
+
+#define BUFSIZE 64
+char inbuf[BUFSIZE];
+char outbuf[BUFSIZE];
 
 int main(int argc, char *argv[])
 {
     int count = 0;
-    int file;
     int adapter_nr = 0; /* probably dynamically determined */
     char devname[20];
     int addr = 0x48; /* The I2C address */
     unsigned char reg = 0x00; /* Device register to access */
-    int rdval;
+    int rval, stat, c, b;
     char buf[10];
+    int servo0pwm = 1500, servo0spddir = 2;
+    int servo1pwm = 1500, servo1spddir = 2;
+    int servo2pwm = 1500, servo2spddir = 2;
+    int servo3pwm = 1500, servo3spddir = 2;
     
     printf("Hello, world!\n");
   
@@ -35,25 +66,185 @@ int main(int argc, char *argv[])
       printf("open i2c slave 0x%02x: succeeded.\n", addr);
 
 
+    
+    rj_setservo(SERVO_0, servo0pwm);
+    usleep(500000);
+    rj_setservo(SERVO_1, servo1pwm);
+    usleep(500000);
+    rj_setservo(SERVO_2, servo2pwm);
+    usleep(500000);
+    rj_setservo(SERVO_3, servo3pwm);
+    usleep(500000);
+
+
+
     while (1) {
-	printf("Another try %d\n", count++);
+	count++;
 
-        printf("Another try %d\n", count++);
-
-
-	/* Using SMBus commands */
-	rdval = i2c_smbus_read_byte_data(file, reg);
-	if (rdval < 0) {
-	  /* ERROR HANDLING: i2c transaction failed */
-	  printf("i2c_smbus_read_byte transaction failed.\n");
-	} else {
-	  printf("i2c_smbus_read_byte value = 0x%02x\n", rdval);
+	if ((count % 10) == 0) {
+	    printf("\33[2K\r%06d:", count);
+	    stat = rj_readstat();
+	    for (b = 7; b >= 0; b--)
+		printf("%c", (stat & (1 << b)) ? '1' : '0');
+	    printf(": ");
+	    fflush(stdout);
+	    
+	    if (stat >= 0) {
+		if (stat & ADCBUSY)
+		    printf("adcbusy:");
+		
+		if (stat & EEBUSY)
+		    printf("eebusy:");
+		
+		if (stat & RXA485) {
+		    rval = rj485_read();
+		    printf("\nrs485: %s: \n", inbuf);
+		}
+		
+		if (stat & RXA232) {
+		    rval = rj232_read();
+		    printf("\nrs232: %s: \n", inbuf);
+		}
+		printf("");
+		
+	    }
 	}
 
+	if ((count % 200) == 0) {
+	    sprintf(outbuf, "Hello 232!!! count = %d\n\r", count);
+	    rj232_send(outbuf, strlen(outbuf));
+	    sprintf(outbuf, "Hello 485!!! count = %d\n\r", count); 
+	    rj485_send(outbuf, strlen(outbuf));
+	}
+	
 
-	usleep(500000);
+	servo0pwm += servo0spddir;
+	if ((servo0pwm > 2000) || (servo0pwm < 1000))
+	    servo0spddir = -servo0spddir;
+	rj_setservo(SERVO_0, servo0pwm);
+
+	servo1pwm += servo1spddir;
+	if ((servo1pwm > 2000) || (servo1pwm < 1000))
+	    servo1spddir = -servo1spddir;
+	rj_setservo(SERVO_1, servo1pwm);
+
+	servo2pwm += servo2spddir;
+	if ((servo2pwm > 2000) || (servo2pwm < 1000))
+	    servo2spddir = -servo2spddir;
+	rj_setservo(SERVO_2, servo2pwm);
+
+	servo3pwm += servo3spddir;
+	if ((servo3pwm > 2000) || (servo3pwm < 1000))
+	    servo3spddir = -servo3spddir;
+	rj_setservo(SERVO_3, servo3pwm);
+
+	usleep(10000);
     }
     
 }
 
-   
+int rj_readbyte(int subreg, const char *caller)
+{
+   int rval, retry = 3;
+    do {
+	rval = i2c_smbus_read_byte_data(file, subreg);
+	if (rval < 0) {
+	    retry--;
+	    usleep(10000);
+	}
+    } while ((rval < 0) && (retry > 0));
+
+    if (rval < 0)
+	printf("%s: i2c_smbus_read_byte retries failed r = 0x%x.\n",
+	       caller, rval);
+    return rval;
+}   
+
+int rj_writebyte(int subreg, int data, const char *caller)
+{
+   int rval, retry = 3;
+    do {
+	rval = i2c_smbus_write_byte_data(file, subreg, data);
+	if (rval < 0) {
+	    retry--;
+	    usleep(10000);
+	}
+    } while ((rval < 0) && (retry > 0));
+
+    if (rval < 0)
+	printf("%s: i2c_smbus_write_byte retries failed r = 0x%x.\n",
+	       caller, rval);
+    return rval;
+}   
+
+int rj_writeword(int subreg, int data, const char *caller)
+{
+   int rval, retry = 3;
+    do {
+	rval = i2c_smbus_write_word_data(file, subreg, data);
+	if (rval < 0) {
+	    printf("X\n");
+	    retry--;
+	    usleep(10000);
+	}
+    } while ((rval < 0) && (retry > 0));
+
+    if (rval < 0)
+	printf("%s: i2c_smbus_write_word retries failed r = 0x%x.\n",
+	       caller, rval);
+    return rval;
+}   
+
+int rj_readstat(void)
+{
+    return rj_readbyte(GSTAT, __func__);
+}
+
+int rj232_read(void)
+{
+    int i = 0;
+    while ((rj_readstat() & RXA232) && (i < BUFSIZE)) {
+	inbuf[i++] = rj232_getc();
+    }
+    inbuf[i] = 0;
+    return i;
+}
+
+int rj485_read(void)
+{
+    int i = 0;
+    while ((rj_readstat() & RXA485) && (i < BUFSIZE)) {
+	inbuf[i++] = rj485_getc();
+    }
+    inbuf[i] = 0;
+    return i;
+}
+
+int rj232_getc(void)
+{
+    return rj_readbyte(RS232D, __func__);
+}
+
+int rj485_getc(void)
+{
+    return rj_readbyte(RS485D, __func__);
+}
+
+int rj232_send(unsigned char *buf, int len)
+{
+    int i, rval;
+    for (i = 0; i < len; i++)
+	rval = rj_writebyte(RS232D, *buf++, __func__);
+} 
+
+int rj485_send(unsigned char *buf, int len)
+{
+    int i, rval;
+    for (i = 0; i < len; i++)
+	rval = rj_writebyte(RS485D, *buf++, __func__);
+}
+
+int rj_setservo(int chan, int usec)
+{
+    return rj_writeword(chan, usec, __func__);
+}
