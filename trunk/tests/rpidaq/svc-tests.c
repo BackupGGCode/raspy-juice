8b2533c05926f1f4a1bd52a540f0d9bd30d1f405
adnan@singnet.com.sg <adnan@singnet.com.sg@18edbb5f-9ea4-c225-5032-a7d48193d222>
2013-06-26 03:25:12 +0000

    Updated svn-tests.c from skeletion of just rj232 services to include TWI command processor to allow resetting into bootloader mode, for easier uploading of new firmware by using twiboot flasher app.

--- trunk/tests/rpidaq/svc-tests.c
+++ trunk/tests/rpidaq/svc-tests.c
@@ -33,7 +33,9 @@
  ***********************************************************************/
 
 #include "juice.h"
+#include <avr/wdt.h>
 #include <avr/interrupt.h>
+#include <util/delay.h>
 
 /* With MCU clock of 14.7456MHz prescaled by 256, period = 34.7us */
 #define DEFAULT_BPS9600_PRESCALER	0b100   // clk/64
@@ -266,16 +268,68 @@ avr-gcc -mmcu=atmega168a -Wall -Os -gdwarf-2 -std=gnu99
 avr-objcopy -O ihex -R .eeprom -R .fuse -R .lock -R .signature \
   svc-tests.elf svc-tests.hex
  */
+FILE rs232_stream = FDEV_SETUP_STREAM(rs232_putchar, rs232_getchar, 
+				      _FDEV_SETUP_RW);
+char const VERSION_STR[] PROGMEM = "$Id:";
+volatile static unsigned char twi_state = 0;
+unsigned char led_state = 1;
+long led_counter = 0, led_timing[4] = { 80000L, 40000L, 20000L, 40000L }; 
+
+void led_heartbeat(void)
+{
+
+    if (led_counter++  > led_timing[led_state]) {
+	led_counter = 0;
+	led_state = ++led_state % 4;
+	if (led_state % 2)
+	    LED_ON();
+	else
+	    LED_OFF();
+    }
+}
+
+#ifdef TWI_POLL_MODE
+# define TWI_debug(fmt, ...)	printf_P(fmt, ##__VA_ARGS__)
+#else
+# define TWI_debug(fmt, ...)
+#endif
+
 int main(void)
 {
     char c;
     
     JUICE_PCBA_PINS_INIT();
+    
     rs232_swuart_init();
+#if 0
+    rs485_init();
+    servo_init();
+#endif
+    /* Enable ADC, and set clock prescaler to div 128 */
+    ADCSRA = (1<<ADEN) | (1<<ADPS2) | (1<<ADPS1) | (1<<ADPS0);
+    
+    TWAR = (AVRSLAVE_ADDR << 1);
+    TWCR = (1<<TWINT) | (1<<TWEA) | (1<<TWEN) | (1<<TWIE);
+    
     sei();
-    rs232_puts("\nTest Application of SW-UART\n\n");
+
+    /* Test printouts */
+    stdout = stdin = &rs232_stream;
+    TWI_debug("\r\nTest Application of Juice Firmware\r\n");
+    TWI_debug("Second line\r\n\r\n");
+    
+
+    
     
     while (1) {
+#if 1
+	led_heartbeat();
+	
+	
+	
+	
+	
+#else
 	/* Testing rs232 echo */
 	if (rs232_havechar()) {
 	    c = rs232_getc();
@@ -284,6 +338,204 @@ int main(void)
 	    }
 	    rs232_putc(c);
 	}
+#endif
+    }
+}
+
+
+ISR(TWI_vect)
+{
+    static unsigned char bcnt, reg, prep_data, lobyte;
+    static int servo_pwm, eeaddr;
+    static const char *pgm_ptr;
+    unsigned char data;
+    
+    twi_state = TWSR;
+    switch (twi_state & 0xF8) {
+	/* TWI SLAVE REGISTER READS */
+    case TWI_STX_ADR_ACK:
+    case TWI_STX_DATA_ACK:
+	TWDR = prep_data;
+	TWCR = (1<<TWEN)| (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWWC);
+	if (reg == ADCDAT) {
+	    prep_data = ADCH;
+	}
+#if 0
+	// TODO: have to re-think how to slave-xmit multiple bytes for
+	// received data with ack/nack. Can't put the below lines because
+	// if master asks for 1 byte only, pre-unload of reception FIFO's
+	// will lose the data. So, for now, the master has to check GSTAT,
+	// followed by single byte read -- a bit inefficient.
+	
+	else if (reg == RS232D) {
+	    if (rs232_havechar())
+		prep_data = rs232_getc();
+	}
+	else if (reg == RS485D) {
+	    if (rs485_havechar())
+		prep_data = rs485_getc();
+	}
+#endif
+	else if (reg == EEDATA) {
+#if 0	    
+	    prep_data = eeprom_read_byte((unsigned char *)eeaddr++);
+#endif
+	}
+	else if (reg == RJ_VERSION) {
+	    prep_data = pgm_read_byte(pgm_ptr++);
+	}
+	break;
+
+    case TWI_STX_DATA_NACK:
+	// Data byte in TWDR has been transmitted; NACK has been received. 
+	// Do nothing
+	// Reset the TWI Interupt to wait for a new event.
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO)| (0<<TWWC);
+	break;     
+
+    case TWI_SRX_GEN_ACK:
+    case TWI_SRX_ADR_ACK:
+	bcnt = 0;
+	// Reset the TWI Interupt to wait for a new event.
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWWC);
+	break;
+
+	/* TWI SLAVE REGISTER WRITES */
+    case TWI_SRX_ADR_DATA_ACK:
+    case TWI_SRX_GEN_DATA_ACK:
+	data = TWDR;
+	switch (++bcnt) {
+	case 0:
+	    break;
+
+	case 1:
+	    reg = data;
+	    
+	    /* For readback registers, prepare data here */
+	    if (reg == GSTAT) {
+		prep_data = 
+		    (rs232_havechar() ? RXA232 : 0) |
+#if 0
+		    (rs485_havechar() ? RXA485 : 0) |
+		    (eeprom_is_ready() ? 0 : EEBUSY) |
+#endif
+		    (ADCSRA & (1<<ADSC) ? ADCBUSY : 0);
+	    }
+	    else if (reg == ADCDAT) {
+		prep_data = ADCL;
+	    }
+	    else if (reg == RS232D) {
+		if (rs232_havechar())
+		    prep_data = rs232_getc();
+	    }
+	    else if (reg == RS485D) {
+#if 0
+		if (rs485_havechar())
+		    prep_data = rs485_getc();
+#endif
+	    }
+	    else if (reg == EEDATA) {
+		#if 0
+		prep_data = eeprom_read_byte((unsigned char *)eeaddr++);
+#endif
+	    }
+	    else if (reg == RJ_VERSION) {
+		prep_data = pgm_read_byte(pgm_ptr);
+	    }
+	    break;
+
+	case 2:
+	    if (reg >= 1 && reg <= 4) {
+		servo_pwm = data;
+	    }
+	    else if (reg == RS232D) {
+		rs232_putc(data);
+	    }
+	    else if (reg == BPS232) {
+		lobyte = data;
+	    }
+	    else if (reg == RS485D) {
+#if 0
+		rs485_putc(data);
+#endif
+	    }
+	    else if (reg == BPS485) {
+		lobyte = data;
+	    }
+	    else if (reg == ADCMUX) {
+		/* Set mux here, force ADLAR bit to zero, and start */
+		/* let user i2c setting to select the volt ref src  */
+		ADMUX = (data & 0b11001111);
+		ADCSRA |= (1 << ADSC);
+	    }
+	    else if (reg == EEADDR) {
+		eeaddr = data;
+	    }
+	    else if (reg == EEDATA) {
+#if 0
+		eeprom_write_byte((unsigned char *)eeaddr, data);
+#endif
+	    }
+	    else if (reg == RJ_VERSION) {
+		pgm_ptr = VERSION_STR;
+	    }
+	    else if ((reg == REBOOT) && (data == BOOTVAL)) {
+		TWCR |= (1<<TWINT) | (1<<TWEA);
+		wdt_enable(WDTO_30MS);
+		while(1) {}; 
+	    }
+	    break;
+
+	default:
+	    if (reg >= SERVO_0 && reg <= SERVO_3) {
+		servo_pwm = (data << 8) | servo_pwm;
+#if 0
+		servo_set(reg - 1, servo_pwm);
+#endif
+	    }
+	    else if (reg == RS232D) {
+		rs232_putc(data);
+	    }
+	    else if (reg == RS485D) {
+#if 0
+		rs485_putc(data);
+#endif
+	    }
+	    else if (reg == BPS232) {
+		rs232_swuart_setbaud(data, lobyte);
+	    }
+	    else if (reg == BPS485) {
+#if 0
+		rs485_setbaud((data << 8) | lobyte);
+#endif
+	    }
+	    else if (reg == EEADDR) {
+		eeaddr = ((data << 8) & 0x01) | eeaddr;
+#if 0
+		prep_data = eeprom_read_byte((unsigned char *)eeaddr++);
+#endif
+	    }
+	    break;
+	}
+	// Reset the TWI Interupt to wait for a new event.
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWWC);
+	break;
+
+    case TWI_SRX_STOP_RESTART:
+	// Enter not addressed mode and listen to address match
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWWC);
+	break;           
+
+    case TWI_SRX_ADR_DATA_NACK:
+    case TWI_SRX_GEN_DATA_NACK:
+    case TWI_STX_DATA_ACK_LAST_BYTE:
+    case TWI_BUS_ERROR:
+	TWCR = (1<<TWSTO) | (1<<TWINT);
+	break;
+
+    default:
+	TWCR = (1<<TWEN) | (1<<TWIE) | (1<<TWINT) | (1<<TWEA) | (0<<TWSTA) | (0<<TWSTO) | (0<<TWWC);
+	break;
     }
 }
 #endif
