4165a87dda0abd5ce7ddf6e530c73c8d937c53dc
adnan@singnet.com.sg <adnan@singnet.com.sg@18edbb5f-9ea4-c225-5032-a7d48193d222>
2013-06-28 02:21:35 +0000

    Added some functionality of RS485 testing.

--- tests/rpidaq/juice-comtests.c
+++ tests/rpidaq/juice-comtests.c
@@ -110,16 +110,17 @@ int test_avr232_comms(int randrun)
     }
 
     /* clean out rj232 buffer */
-    printf("rj232_read: cleaning out rj232 read buffer.\n");
     while(rj_readstat() & RXA232) {
 	rj232_read((unsigned char *)avr_inbuf, CT_BUFSIZE);
     }
     
+    printf("AVR RS232 Egress Test\n");
     if (test_avr232_egress(1)) {
 	printf("AVR RS232 Egress: failed\n");
 	return -5;
     }
     
+    printf("AVR RS232 Ingress Test\n");
     if (test_avr232_ingress(1)) {
 	printf("AVR RS232 Ingress: failed\n");
 	return -6;
@@ -178,7 +179,7 @@ int test_con232_comms(int randrun)
     printf("Console RS232 Egress Test\n");
     daq_set_comms_matrix(RPITXD_TO_DUT_HDR_TXD | RPIRXD_FROM_CONSOLE_TX);
     if (test_console_echo(0)) {
-	printf("test_console_echo:  egress failed.\n");
+	printf("Console RS232 egress:  failed.\n");
 	return -2;
     }
 	
@@ -186,7 +187,7 @@ int test_con232_comms(int randrun)
     printf("Console RS232 Ingress Test\n");
     daq_set_comms_matrix(RPITXD_TO_CONSOLE_RX | RPIRXD_FROM_DUT_HDR_RXD);
     if (test_console_echo(1)) {
-	printf("test_console_echo: ingress failed.\n");
+	printf("Console RS232 ingress: failed.\n");
 	return -3;
     }
 
@@ -194,14 +195,128 @@ int test_con232_comms(int randrun)
     return 0;
 }
 
+/*
+ * Tests the auxiliary RS232 interface of the AVR microcontroller on Juice
+ * The interface is a HALF Duplex, so it cannot simultaneously send/receive
+ * data. A wait in between the transactions is neccessary.
+ */
+int test_avr485_comms(int randrun)
+{
+    int r, retries, out_n, avr_in, rpi_in;
+    
+    rpi_comport_setbaud(B115200);
+    
+    if (rj485_setbaud(115200)) {
+	printf("%s: rj485_setbaud failed.\n", __func__);
+	return -2;
+    }
+
+    /* flush rj485 buffer */
+    while(rj_readstat() & RXA485) {
+	rj485_read((unsigned char *)avr_inbuf, CT_BUFSIZE);
+    }
+
+/*
+#define RPIRXD_FROM_DUT_HDR_RXD	0x01
+#define RPIRXD_FROM_CONSOLE_TX	0x02
+#define RPIRXD_FROM_AVR232_TX	0x03
+#define RPIRXD_FROM_AVR485_TX	0x04
+
+#define RPITXD_TO_DUT_HDR_TXD	0x10
+#define RPITXD_TO_CONSOLE_RX	0x20
+#define RPITXD_TO_AVR232_RX	0x30
+#define RPITXD_TO_AVR485_RX	0x40
+*/
+    printf("AVR RS485 Egress Test\n");
+    daq_set_comms_matrix(RPIRXD_FROM_AVR485_TX);
+    sprintf(avr_outbuf, "RPi >> I2C >> DUT RS485 >> xAA55aa55x >> RPi RS485 RxD");
+    out_n = strlen(avr_outbuf);
+    rj485_send((unsigned char *) avr_outbuf, out_n);
+    
+    memset(avr_inbuf, 0, CT_BUFSIZE);
+    memset(rpi_inbuf, 0, CT_BUFSIZE);
+    avr_in = rpi_in = 0;
+    retries = 10;
+    
+    while (retries--) {
+	while (rj_readstat() & RXA485) {
+	    r = rj232_read((unsigned char *)avr_inbuf, CT_BUFSIZE);
+	    avr_in += r;
+	}
+	
+	r = read(fd_comport, &rpi_inbuf[rpi_in], CT_BUFSIZE - rpi_in - 1);
+	if (r > 0) {
+	    rpi_in += r;
+	}
+	msleep(10);
+    }
+    rpi_inbuf[rpi_in] = 0;
+    
+    printf("avr_outbuf: %02d bytes: %s\n", out_n,  avr_outbuf);
+    printf("avr_inbuf:  %02d bytes: %s\n", avr_in, avr_inbuf);
+    printf("rpi_inbuf:  %02d bytes: %s\n", rpi_in, rpi_inbuf);
+    
+    /* Egress comparisons */
+
+    
+    
+    
+    
+
+    /* AVR RS485 Ingress TEST */
+    
+    printf("AVR RS485 Ingress Test\n");
+    daq_set_comms_matrix(RPITXD_TO_AVR485_RX);
+    sprintf(rpi_outbuf, "RPi >> RS485 TxD >> xAA55aa55x >> DUT RS485 >> I2C >> RPi");
+    out_n = strlen(rpi_outbuf);
+    
+    /* flush rj485 buffer */
+    while(rj_readstat() & RXA485) {
+	rj485_read((unsigned char *)avr_inbuf, CT_BUFSIZE);
+    }
+
+    write(fd_comport, rpi_outbuf, out_n);
+
+
+    memset(avr_inbuf, 0, CT_BUFSIZE);
+    memset(rpi_inbuf, 0, CT_BUFSIZE);
+    avr_in = rpi_in = 0;
+    retries = 10;
+    
+    while (retries--) {
+	while (rj_readstat() & RXA485) {
+	    r = rj485_read((unsigned char *)avr_inbuf, CT_BUFSIZE);
+	    avr_in += r;
+	}
+	
+	r = read(fd_comport, &rpi_inbuf[rpi_in], CT_BUFSIZE - rpi_in - 1);
+	if (r > 0) {
+	    rpi_in += r;
+	}
+	msleep(10);
+    }
+    rpi_inbuf[rpi_in] = 0;
+    
+    printf("rpi_outbuf: %02d bytes: %s\n", out_n,  rpi_outbuf);
+    printf("avr_inbuf:  %02d bytes: %s\n", avr_in, avr_inbuf);
+    printf("rpi_inbuf:  %02d bytes: %s\n", rpi_in, rpi_inbuf);
+    
+    daq_set_comms_matrix(0);
+   
+    
+    
+    return 0;
+}
+
+
 
 int test_juice_comms(int randrun)
 {
     if (test_con232_comms(1))
 	return -1;
-#if 1
     if (test_avr232_comms(1))
 	return -2;
-#endif
+    if (test_avr485_comms(1))
+	return -3;
     return 0;
 }
