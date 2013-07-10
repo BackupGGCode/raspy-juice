f6fd4c2ffff1277aa0c566b4369ff1ceaf29e96c
adnan@singnet.com.sg <adnan@singnet.com.sg@18edbb5f-9ea4-c225-5032-a7d48193d222>
2013-07-10 11:04:37 +0000

    Updated dates in source files.

--- trunk/firmware/i2c-master.c
+++ trunk/firmware/i2c-master.c
@@ -5,7 +5,7 @@
  *
  *
  *
- * Copyright (c) 2012, Adnan Jalaludin <adnan singnet.com.sg>
+ * Copyright (c) 2012-2013, Adnan Jalaludin <adnan singnet.com.sg>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
--- trunk/firmware/svc-servo.c
+++ trunk/firmware/svc-servo.c
@@ -5,7 +5,7 @@
  * MCU: ATmega168A, 14.7456MHz
  *
  *
- * Copyright (c) 2012, Adnan Jalaludin <adnan singnet.com.sg>
+ * Copyright (c) 2012-2013, Adnan Jalaludin <adnan singnet.com.sg>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
